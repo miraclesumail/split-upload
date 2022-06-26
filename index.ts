c10e22057e5d47e308a4382ecab58de82fa97b76
leibin <857247710@qq.com>
2022-06-27 00:49:03 +0800

    feat: 提交

--- index.ts
+++ index.ts
@@ -23,6 +23,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post("/upload", upload.any(), uploadFiles);
 
+app.post('/mergeChunks', mergeChunks)
+
 function uploadFiles(req: Request, res: Response) {
     console.log(req.body, 'req.body');
     res.json({ message: "Successfully uploaded files" });
@@ -30,8 +32,13 @@ function uploadFiles(req: Request, res: Response) {
 
 app.listen(8888, function () {
     console.log(`the server is start at port 8888`);
-    mergeFileChunk()
 });
+
+async function mergeChunks(req: Request, res: Response) {
+    const { size, filename } = req.body;
+    await mergeFileChunk(size, filename);
+    res.json({ message: "Successfully combined files" });
+}
 
 // 通过管道处理流 
 // @ts-ignore
@@ -46,22 +53,22 @@ const pipeStream = (path, writeStream) => {
     });
 }
 
-const mergeFileChunk = async (size = 30000) => {
+const mergeFileChunk = async (size = 30000, filename: string) => {
     const chunkDir = path.join(__dirname, './uploads');
-    const chunkPaths = fs.readdirSync(chunkDir);
+    let chunkPaths = fs.readdirSync(chunkDir);
 
     if (!chunkPaths.length) return;
 
     // 根据切片下标进行排序，否则直接读取目录的获得的顺序可能会错乱
-    chunkPaths.sort((a, b) => Number(a.split("-")[2]) - Number(b.split("-")[2]));
-    console.log("chunkPaths = ", chunkPaths);
+    chunkPaths = chunkPaths.filter(filePath => filePath.includes(filename)).sort((a, b) => Number(a.split("-")[2]) - Number(b.split("-")[2]));
+    console.log("chunkPaths = ", chunkPaths, filename);
 
     await Promise.all(
         chunkPaths.map((chunkPath, index) =>
             pipeStream(
                 path.resolve(chunkDir, chunkPath),
                 // 指定位置创建可写流
-                fs.createWriteStream(path.join(__dirname, './uploads/1.png'), {
+                fs.createWriteStream(path.join(__dirname, `./uploads/${filename}`), {
                     start: index * size,
                 })
             )
